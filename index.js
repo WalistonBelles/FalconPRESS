ec896f908f999145cb1a3efbc2864c8d3476e34c
Waliston Belles <walistonbelles@bol.com.br>
2021-04-23 10:13:36 -0300

    Adicionado estrutura básica

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const connection = require('./database/database');
+
+// View Engine
+app.set('view engine', 'ejs');
+
+// Static
+app.use(express.static('public'));
+
+// Body Parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// Database
+connection
+    .authenticate()
+    .then(() =>{
+      console.log('Conexão realizada com sucesso!');  
+    }).catch((error) => {
+        console.log(error);
+    });
 
 app.get("/", (req, res) => {
     res.send("Bem vindo ao site");
